ce1533e2bd9c7a92a85e1101616f35ea9e5cf0d4
Abhijit808 <abhijitrayarao@gmail.com>
2023-02-03 20:42:25 +0530

    change in layout

--- components/ArtGallery/ArtGallery.tsx
+++ components/ArtGallery/ArtGallery.tsx
@@ -4,33 +4,33 @@ import React from 'react'
 const ArtGallery = () => {
   return (
     <>
-    <div className='grid grid-cols-3 gap-3'>
+    <div className='grid grid-cols-3 gap-3 cursor-pointer'>
         {/* <Image src = "/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg" alt='WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg'
         height={500}
         width={500}
         /> */}
-       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg"></img>
+       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg " className='hover:-translate-y-3'></img>
        <img src="/images/WhatsApp Image 2021-03-24 at 2.22.01 PM (1).jpeg"
-        className='row-span-2 object-cover h-full'
+        className='row-span-2 object-cover h-full hover:-translate-y-3'
         ></img>
-       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg"></img>
-       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg"></img>
+       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg"className='hover:-translate-y-3'></img>
+       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg"className='hover:-translate-y-3'></img>
        <img src="/images/WhatsApp Image 2021-03-24 at 2.22.01 PM.jpeg"
-        className='row-span-2 object-cover h-full'
+        className='row-span-2 object-cover h-full hover:-translate-y-3'
         ></img>
-       <img src="/images/WhatsApp Image 2021-08-13 at 3.01.59 PM.jpeg"></img>
-       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg" className='row-span-2 object-cover h-full'></img>
+       <img src="/images/WhatsApp Image 2021-08-13 at 3.01.59 PM.jpeg"className='hover:-translate-y-3'></img>
+       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg" className='row-span-2 hover:-translate-y-3 object-cover h-full'></img>
        <img src="/images/WhatsApp Image 2021-08-13 at 3.02.01 PM.jpeg"
-        className='row-span-2 object-cover h-full'
+        className='row-span-2 object-cover h-fullhover:-translate-y-3'
         ></img>
-       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg"></img>
-       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg" className='h-full object-cover'></img>
-       <img src="/images/WhatsApp Image 2021-08-13 at 3.02.02 PM.jpeg"></img>
+       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg"className='hover:-translate-y-3'></img>
+       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg" className='hover:-translate-y-3 h-full object'></img>
+       <img src="/images/WhatsApp Image 2021-08-13 at 3.02.02 PM.jpeg"className='hover:-translate-y-3'></img>
        <img src="/images/WhatsApp Image 2021-08-13 at 3.02.03 PM.jpeg"
-        className='row-span-2 object-cover h-full'
+        className='row-span-2 object-cover h-full hover:-translate-y-3'
         ></img>
-       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg"></img>
-       <img src="/images/WhatsApp Image 2021-08-13 at 3.02.03 PM.jpeg"></img>
+       <img src="/images/WhatsApp Image 2021-03-24 at 2.21.59 PM (1).jpeg"className='hover:-translate-y-3'></img>
+       <img src="/images/WhatsApp Image 2021-08-13 at 3.02.03 PM.jpeg"className='hover:-translate-y-3'></img>
     </div>
     </>
   )
